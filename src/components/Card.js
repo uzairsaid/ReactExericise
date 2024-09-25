f6335090b9b3151ce517a472d44bf1e9f7d63ffb
Uzair Said <bitanguzair@gmail.com>
2024-09-25 16:07:44 +0200

    Added icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,18 +18,18 @@ function Card({title}){
         {data?
                 (<div className="card-body">
                 
-                <p>Country : {data.sys.country}</p>
-                <p>Name : {data.name}</p>
-                <p>Weather : {data.weather[0].main}</p>
-                <p>Weather Description: {data.weather[0].description}</p>
-                <p>Wind Speed: {data.wind.speed}</p>
-                <p>Temperature: {data.main.temp}K </p>
-                <p>Minimum temperature: {data.main.temp_min}K </p>
-                <p>Maximum temperature: {data.main.temp_max}K </p>
-                <p>Humidity: {data.main.humidity} %</p>
-                <p>Atmopheric pressure: {data.main.pressure} hPa</p>
-                <p>Sea Level pressure: {data.main.sea_level} hPa</p>
-                <p>Ground Level pressure: {data.main.grnd_level} hPa</p>
+                <p><i className="fa-solid fa-flag"></i>Country : {data.sys.country}</p>
+                <p><i className="fa-solid fa-home"></i> Name : {data.name}</p>
+                <p><i className="fa-solid fa-cloud"></i>Weather : {data.weather[0].main}{data.weather[0].main ==="Rain"?(<i className="fa-solid fa-rain"></i>):data.weather[0].main ==="Clouds"?(<i className="fa-solid fa-cloud"></i>):(<i className="fa-solid fa-sun"></i>)}</p>
+                <p><i className="fa-solid fa-cloud"></i>Weather Description: {data.weather[0].description}</p>
+                <p><i className="fa-solid fa-wind"></i>Wind Speed: {data.wind.speed}</p>
+                <p> <i className="fa-solid fa-temperature-half"></i>Temperature: {(parseFloat(data.main.temp)-273.15).toFixed(2)}Celisius </p>
+                <p> <i className="fa-solid fa-temperature-low"></i> Minimum temperature: {(parseFloat(data.main.temp_min)-273.15).toFixed(2)}Celisius </p>
+                <p> <i className="fa-solid fa-temperature-high"></i> Maximum temperature: {(parseFloat(data.main.temp_max)-273.15).toFixed(2)}Celisius </p>
+                <p><i className="fa-solid fa-snowflake"></i> Humidity: {data.main.humidity} %</p>
+                <p><i className="fa-solid fa-star"></i> Atmopheric pressure: {data.main.pressure} hPa</p>
+                <p><i className="fa-solid fa-water"></i> Sea Level pressure: {data.main.sea_level} hPa</p>
+                <p><i className="fa-solid fa-globe"> </i> Ground Level pressure: {data.main.grnd_level} hPa</p>
 
             </div>)
             : 
