c2ba6948cf3b050958ba854c9db1ccccad055eeb
Uzair Said <bitanguzair@gmail.com>
2024-10-21 13:46:10 +0200

    app modified

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-
-import './App.scss';
-import Card from './components/Card';
-import Content from './components/Content';
-import Footer from './components/Footer';
-import Header from './components/Header'
-import SlideCard from './components/SlideCard';
-import { DataProvider } from './contexts/DataContext';
+import "./App.scss";
+import Card from "./components/Card";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import SlideCard from "./components/SlideCard";
+import { DataProvider } from "./contexts/DataContext";
 
 function App() {
   const info = "Weather information";
   return (
     <div>
-        <DataProvider>
-              <Header/>
-              <div className='container'>
-                <SlideCard/>
-                <Card title={info}/>
-              </div>
-              <Content/>
-              <Footer/>
-        </DataProvider>
-     
+      <DataProvider>
+        <Header />
+        <div className="container">
+          <SlideCard />
+          <Card title={info} />
+        </div>
+        <Content />
+        <Footer />
+      </DataProvider>
     </div>
-
   );
 }
 
