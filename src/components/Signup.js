12ca17f897fe82a8ad7372963b0dad81126e1ccd
Uzair Said <bitanguzair@gmail.com>
2024-10-28 13:43:53 +0200

    Added form validation with react hook form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import "../style/signup.scss";
+import { useForm } from "react-hook-form";
 
 function Signup() {
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confPassword, setConfPassword] = useState("");
+  // const [userName, setUserName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [confPassword, setConfPassword] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  function handleSubmit(e) {
+  function onSubmit(e) {
     e.preventDefault();
     alert("bonjpour");
     console.log("ca marche");
-    console.log(userName);
   }
 
   return (
@@ -20,44 +25,50 @@ function Signup() {
         <h2>Signup form</h2>
       </div>
       <div className="card-body">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
             <label>Username</label>
 
             <input
               type="text"
-              name="username"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
+              {...register("username", { required: "Username is required" })}
             />
+            {errors.username && (
+              <p className="errors">{errors.username.message}</p>
+            )}
           </div>
           <div>
             <label>E mail</label>
             <input
               type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              autoComplete="false"
+              {...register("email", { required: "E mail is required" })}
             />
+            {errors.email && <p className="errors">{errors.email.message}</p>}
           </div>
           <div>
             <label>Password</label>
             <input
               type="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              {...register("password", {
+                required: "Password is required",
+              })}
             />
+            {errors.password && (
+              <p className="errors">{errors.password.message}</p>
+            )}
           </div>
+
           <div>
             <label>Confirm password</label>
             <input
               type="password"
-              name="confpassword"
-              value={confPassword}
-              onChange={(e) => setConfPassword(e.target.value)}
+              {...register("confPassword", {
+                required: "Password confirmation  is required",
+              })}
             />
+            {errors.confPassword && (
+              <p className="errors">{errors.confPassword.message}</p>
+            )}
           </div>
           <div>
             <button type="submit">Signup</button>
